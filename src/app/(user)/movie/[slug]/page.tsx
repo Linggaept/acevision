5afb413488aa2b543873271c5e9d867af4253940
linggaept <linggaept@gmail.com>
2025-08-11 12:46:41 +0700

    user : fix promise slug props

--- src/app/(user)/movie/[slug]/page.tsx
+++ src/app/(user)/movie/[slug]/page.tsx
@@ -1,6 +1,11 @@
+// src/app/(user)/movie/[slug]/page.tsx
 import MovieDetailClient from "@/components/movies/movie-detail";
 
-export default async function DetailMovie({ params }: { params: { slug: string } }) {
+interface PageProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function DetailMovie({ params }: PageProps) {
   const { slug } = await params;
   return <MovieDetailClient slug={slug} />;
 }
